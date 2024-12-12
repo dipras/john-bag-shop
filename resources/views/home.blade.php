6cee67dabb7e394ab945d7ed4bccc7ee03e21a13
Aldy Prastyo <dipras75@gmail.com>
2024-12-12 18:26:42 +0700

    Feat: Add logout functionality to home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,13 @@
     <div class="w-[300px] bg-[#FEFFFE] p-2">
         <h1 class="text-center text-2xl font-bold text-[#434B6A]">{{ $web_title }}</h1>
     </div>
-    <div class="flex-1 bg-[#F3F3F3] h-[100vh]">
+    <div class="flex-1 bg-[#F3F3F3] h-[100vh]  pl-2">
+        <div class="h-[50px] bg-white flex flex-row justify-end px-5 items-center">
+            <div class="flex gap-2">
+                <x-tabler-logout />
+                <a href="/auth/logout" class="text-[#808080]">LOGOUT</a>
+            </div>
+        </div>
         @yield("home_content")
     </div>
 </div>
