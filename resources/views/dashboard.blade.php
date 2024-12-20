a38aa04f4c61d0f7c1335f640464520ebb9eb791
Aldy Prastyo <dipras75@gmail.com>
2024-12-20 19:54:56 +0700

    Feat: laporan icons to dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,8 +9,8 @@
         <div class="h-[200px] bg-white border-2 border-[#434B6A] rounded shadow-xl relative">
             <x-bi-box class="h-24 w-24 absolute right-[20px] top-[50px]" fill="#434B6A" />
         </div>
-        <div class="h-[200px] bg-white border-2 border-[#434B6A] rounded shadow-xl">
-
+        <div class="h-[200px] bg-white border-2 border-[#434B6A] rounded shadow-xl relative">
+            <x-bi-calendar-week class="h-24 w-24 absolute right-[20px] top-[50px]" fill="#434B6A" />
         </div>
 
     </div>
