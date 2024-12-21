59ae29c6e61f5304a5096537d92b6ce37177f427
Aldy Prastyo <dipras75@gmail.com>
2024-12-21 22:48:06 +0700

    Feat: Create Product model, migration, and factory; fix foreign key constraint for users

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,10 +5,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Category;
 use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Product extends Model
 {
-    protected $table = 'category';
+    use HasFactory;
+
+    protected $table = 'product';
     public function category()
     {
         return $this->belongsTo(Category::class, 'category_id');
